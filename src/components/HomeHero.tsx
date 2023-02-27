abc5cd380ce2167e6c44a77c2dfb51fe81581e10
parz3val <iamtheparzival@gmail.com>
2023-02-27 17:01:00 +0545

    do: profile picture from gitub

--- src/components/HomeHero.tsx
+++ src/components/HomeHero.tsx
@@ -2,27 +2,27 @@
 import Link from "next/link";
 import Image from "next/image";
 export default function HomeHero() {
-    return (
+  return (
     <>
       <div className="flex flex-col items-center justify-center h-full">
-      <h2 className="text-2xl font-fira-code font-bold">
-      Hello, world !!!
-      </h2>
-      <h1 className="text-6xl font-fira-code font-bold">
-      {"I'm Harry"}
-      </h1>
-      <Image src="/images/profile.png" alt="profile" width="64" height="64" className="rounded-full h-64 w-64"/>
-      <h3 className="text-3xl font-fira-code font-bold">
-      Software Engineer/CS Enthusiast
-      </h3>
-      <p className="text-xl font-fira-code max-w-xl text-center justify-center">
-      experienced in voip and embedded. Prefers working with typescript (react), python, rust and golang.
-      </p>
-      <Link href="/cv" passHref>
-      <span className="text-fira-code text-bold">#goto-cv</span>
-      </Link>
+        <h2 className="text-2xl font-fira-code font-bold">
+          Hello, world !!!
+        </h2>
+        <h1 className="text-6xl font-fira-code font-bold">
+          {"I'm Harry"}
+        </h1>
+        <Image src="https://github.com/parz3val.png" alt="profile" width="64" height="64" className="rounded-full h-64 w-64" />
+        <h3 className="text-3xl font-fira-code font-bold">
+          Software Engineer/CS Enthusiast
+        </h3>
+        <p className="text-xl font-fira-code max-w-xl text-center justify-center">
+          experienced in voip and embedded. Prefers working with typescript (react), python, rust and golang.
+        </p>
+        <Link href="/cv" passHref>
+          <span className="text-fira-code text-bold">#goto-cv</span>
+        </Link>
       </div>
-    
+
     </>
-    );
+  );
 }
